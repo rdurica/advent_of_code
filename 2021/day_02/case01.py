1bf0e216e3178248b01879080f6e1f5fd008bfd8
Robert Durica <r.durica@gmail.com>
2021-12-02 21:24:15 +0100

    2021: Day02 Case01

---
+++ 2021/day_02/case01.py
@@ -0,0 +1,44 @@
+# https://adventofcode.com/2021/day/2
+
+
+class Possition:
+
+    depth, horizontal_pos = 0, 0
+
+    def move(self, direction: str, units: int) -> None:
+        if direction == "down":
+            self._move_down(units)
+        if direction == "forward":
+            self._move_forward(units)
+        if direction == "up":
+            self._move_up(units)
+
+    @property
+    def actual_coordinates(self) -> int:
+        return self.depth * self.horizontal_pos
+
+    def _move_forward(self, units: int) -> None:
+        self.horizontal_pos += units
+
+    def _move_up(self, units: int) -> None:
+        self.depth -= units
+
+    def _move_down(self, units: int) -> None:
+        self.depth += units
+
+
+class Parser:
+    def data_generator(self) -> tuple[str, int]:
+        with open("input_data.txt") as stream:
+            for line in stream:
+                direction, units = line.split(" ")
+                yield direction, int(units)
+
+
+parser = Parser()
+position = Possition()
+
+for direction, units in parser.data_generator():
+    position.move(direction, units)
+
+print(f"Result: {position.actual_coordinates}")
